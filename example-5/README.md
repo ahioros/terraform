3e267829b9a7d56074403b877974753ac018beb0
Guillermo Garcia <ahioros@users.noreply.github.com>
2024-01-21 20:20:41 -0500

    fixed providers template

---
+++ example-5/README.md
@@ -0,0 +1,3 @@
+# example-5
+challenge:
+https://medium.com/@ikunalsingh/terraform-challenges-6d2c7938f761
--- template/provider.tf
+++ template/provider.tf
@@ -1,4 +1,15 @@
+terraform {
+  required_version = ">= 1.0.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.33.0"
+    }
+  }
+}
 provider "aws" {
+
   endpoints {
     apigateway     = "http://localhost:4566"
     cloudformation = "http://localhost:4566"
